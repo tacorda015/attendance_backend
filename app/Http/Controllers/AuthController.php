f555f539e93eaa66bfe08c28a6550602f3a03de2
tacorda015 <eduardotacorda17@gmail.com>
2025-02-15 11:40:45 +0800

    Add Login Function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -13,6 +14,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only('username', 'password');
+        Log::alert('Attempt Login');
 
         $request->validate([
             'username' => 'required|string',
@@ -39,8 +41,39 @@ class AuthController extends Controller
             return response()->json(['error' => 'could_not_create_token' . $e->getMessage()], 500);
         }
 
+        Log::info($user->first_name . ' ' . $user->last_name . ': Login');
+
         return response()->json([
             'token' => $token,
         ]);
     }
+
+    public function logout(Request $request)
+    {
+        try {
+            $token =JWTAuth::getToken();
+
+            if (!$token) {
+                return response()->json(['error' => 'Token not provided'], 400);
+            }
+
+            try {
+                // Authenticate the token to get the user
+                $user = JWTAuth::parseToken()->authenticate();
+
+                // Log the user's first name (f_name)
+                Log::info($user->f_name . ' ' . $user->l_name . ': Logout');
+            } catch (JWTException $e) {
+                return response()->json(['error' => 'Token expired or invalid'], 401);
+            }
+
+            JWTAuth::invalidate($token);
+
+            return response()->json(['message' => 'Successfully logged out']);
+
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not invalidate token: ' . $e->getMessage()], 500);
+        }
+    }
+
 }
